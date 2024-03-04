ea0f9e2b9e3f33050ea1befe2103b1b84f00b062
cindyh2 <143455603+cindyh2@users.noreply.github.com>
2024-03-03 23:54:00 -0500

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,22 @@
+//
+//  main.cpp
+//  lab 1 question 1
+//
+//  Created by Cindy Hernandez on 2/26/24.
+//
+
+#include <iostream>
+using namespace std;
+
+int main() {
+  
+    cout << "***************************************************\n" << endl;
+    cout << "Welcome!" << endl;
+    cout << "Please choose a number from the following options: " << endl;
+    cout << " 1. Play the game!" << endl;
+    cout << " 2. Demo the game!" << endl;
+    cout << " 3. Exit \n" << endl;
+    cout << "***************************************************" << endl;
+    
+    return 0;
+}
